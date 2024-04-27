c46b714a842a358cfb6628a92dbbd6b2e6adb1f3
!CyberApex <46587415+ApexNAM@users.noreply.github.com>
2024-04-27 16:27:53 +0900

    Update CarAIController.gd
    설명 추가

--- CarAIController.gd
+++ CarAIController.gd
@@ -26,26 +26,33 @@ func dump_first_physics_frame() -> void:
 	set_physics_process(true)
 
 func _process(delta):
+	# 목적지에 거의 도달하면 위치를 변경합니다.
 	if global_transform.origin.distance_to(randomPoint) < 10:
 		randomPoint = Vector3(randf_range(-35.0, 35.0), global_position.y, randf_range(-35.0, 35.0))
 
 func _physics_process(delta):
 	var target = randomPoint
-	pickup_processing(target)
-	
+	pickup_processing(target) # 위치 설정
+
 	var drive_input = 1
 	engine_force = lerpf(engine_force, drive_input * horsepower, acceleration * delta)
-	
+
+	# 목적지 이동에 맞게, 핸들이 변경됩니다.
 	var steer_input = angle_dir(global_transform.basis.z, targetDir.normalized(), global_transform.basis.y)
 	steering = steer_input * steer_limit
-		
+
+	# 왼쪽에 벽이 있을 경우 오른쪽으로 이동합니다.
+	# 오른쪽에 벽이 있을 경우 왼쪽으로 이동합니다.
 	if IsLeft():
 		if steering != (-45 * steer_limit):
 			steering = -45 * steer_limit
 	elif IsRight():
 		if steering != (45 * steer_limit):
 			steering = 45 * steer_limit
-			
+	
+	# 모든 Raycast Left가 감지되면, 오른쪽으로 돌리며, 후진합니다.
+	# 모든 Raycast Right가 감지되면, 왼쪽으로 돌리며, 후진합니다.
+
 	if IsLeftAll():
 		if steering != (45 * steer_limit):
 			steering = 45 * steer_limit
@@ -54,16 +61,18 @@ func _physics_process(delta):
 		if steering != (-45 * steer_limit):
 			steering = -45 * steer_limit
 		engine_force = -120
-	elif IsLeftAll() and IsRightAll():
+	elif IsLeftAll() and IsRightAll(): # 모두 감지하면 후진합니다.
 		print("All")
 		engine_force = -120
 		
 	print(name + ", L : " + str(IsLeftAll()))
 	print(name + ", R : " + str(IsLeftAll()))
-	
+
+	# 벽에 부딫치면 후진합니다.
 	if brake_check.is_colliding():
 		engine_force = -80
-					
+
+# 위치 설정
 func pickup_processing(target) -> void:
 	navAgent.set_target_position(target)
 	var nextPathPoint = navAgent.get_next_path_position()
